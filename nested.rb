36cae78e6f63b6ea123cd283a5c96571f08935c7
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 18:33:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,7 @@ def dennis_ritchies_language
         }
      }
      programmer_hash[:dennis_ritchie][:languages][0]
+     here you have to enter it as an array so that you can take out the "" otherwise you tried to hardcode. 
 end 
 
 def adding_matz
