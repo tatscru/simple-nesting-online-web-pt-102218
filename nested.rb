c62f873cc3e28fb4bccd689dbd165fe75a6069c4
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-04 18:24:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
 end
 
 def adding_matz
@@ -82,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]
     
 end
 
